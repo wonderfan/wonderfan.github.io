98bb137a848b9896852fda7397e6a37089201d75
阿樊提 <fanjiahe2000@163.com>
2015-06-22 15:23:28 +0800

    Update app.js

--- ui/angularjs/js/app.js
+++ ui/angularjs/js/app.js
@@ -1,11 +1,11 @@
-var myApp = angular.module('myApp', ['infinite-scroll']);
+var myApp = angular.module('myApp', ['infinite-scroll']);//create the module and declare the depedency
 myApp.controller('MyController', function($scope) {
   $scope.images = [1, 2, 3, 4, 5, 6, 7, 8];
 
   $scope.loadMore = function() {
-    var last = $scope.images[$scope.images.length - 1];
+    var last = $scope.images[$scope.images.length - 1];//get the image length
     for(var i = 1; i <= 8; i++) {
-      $scope.images.push(last + i);
+      $scope.images.push(last + i);// insert another new 8 elements
     }
   };
 });
