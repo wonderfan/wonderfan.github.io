1a5a80c139284f3a6eee17cb88663859fdd7d098
wonderfan <fanjiahe2000@163.com>
2016-03-08 11:14:46 +0800

    Update metal.js

--- ui/angularjs/metal.js
+++ ui/angularjs/metal.js
@@ -1,6 +1,1 @@
-
-  function metalController($scope){
-     $scope.name = "it is metal";
-  }
-  metalController.$inject = ['$scope'];
-  angular.module('MyApp').controller('metalController',metalController);
+aa
